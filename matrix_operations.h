eb69e7354704f774b1a1d8adc3bc17733c1bbeb8
Vitalina <linakimarmy@mail.ru>
2024-04-06 00:35:40 +1000

    Update matrix_operations.h

--- matrix_operations.h
+++ matrix_operations.h
@@ -1,4 +1,1 @@
-double **matrix_addition(double **matrix1, double **matrix2, int n);
-double **matrix_subtraction(double **matrix1, double **matrix2, int n);
-double **matrix_multiplication(double **matrix1, double **matrix2, int n);
-
+double** matrix_operation(double** matrix1, double** matrix2, int n, char operation);
