404d034c239c5dfc368733d3b73e4ff2ff263ead
Vitalina <linakimarmy@mail.ru>
2024-04-06 00:35:30 +1000

    Update matrix_operations.c

--- matrix_operations.c
+++ matrix_operations.c
@@ -1,39 +1,26 @@
-#include <stdio.h>
-#include "matrix_operations.h"
+#include <stdlib.h>
 
-double **matrix_add(double **matrix1, double **matrix2, int size) {
-	double **result = (double **)malloc(size * sizeof(double *));
-    for (int i = 0; i < size; i++) {
-        result[i] = (double *)malloc(size * sizeof(double));
-        for (int j = 0; j < size; j++) {
-            result[i][j] = matrix1[i][j] + matrix2[i][j];
-        }
+double** matrix_operation(double** matrix1, double** matrix2, int n, char operation) {
+    int i,j;
+	double** result = (double**)malloc(n * sizeof(double*));
+    for (i = 0; i < n; i++) {
+        result[i] = (double*)malloc(n * sizeof(double));
     }
-    return result;
-}
 
-double **matrix_min(double **matrix1, double **matrix2, int size) {
-    double **result = (double **)malloc(size * sizeof(double *));
-    for (int i = 0; i < size; i++) {
-        result[i] = (double *)malloc(size * sizeof(double));
-        for (int j = 0; j < size; j++) {
-            result[i][j] = matrix1[i][j] - matrix2[i][j];
-        }
-    }
-    return result;
-}
-
-double **matrix_mult(double **matrix1, double **matrix2, int size) {
-    double **result = (double **)malloc(size * sizeof(double *));
-    for (int i = 0; i < size; i++) {
-        result[i] = (double *)malloc(size * sizeof(double));
-        for (int j = 0; j < size; j++) {
-            result[i][j] = 0;
-            for (int k = 0; k < size ; k++) {
-                result[i][j] += matrix1[i][k] * matrix2[k][j];
+    for (i = 0; i < n; i++) {
+        for (j = 0; j < n; j++) {
+            if (operation == '+') {
+                result[i][j] = matrix1[i][j] + matrix2[i][j];
+            } else if (operation == '-') {
+                result[i][j] = matrix1[i][j] - matrix2[i][j];
+            } else if (operation == '*') {
+                result[i][j] = matrix1[i][j] * matrix2[i][j];
             }
         }
     }
+
     return result;
 }
+
+
 
