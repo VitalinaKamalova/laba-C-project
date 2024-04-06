39d03f72963c83bddc25b946690533aea65201bb
Vitalina <151424991+VitalinaKamalova@users.noreply.github.com>
2024-04-06 13:07:52 +1000

    Update laba5.c

--- laba5.c
+++ laba5.c
@@ -3,52 +3,49 @@
 #include <stdlib.h>
 #include "matrix_operations.h"
 
-int main() {
-    int n,i,j;
-    char operation;
-    printf("vvedite razmernost' matrizi n: ");
-    scanf("%d", &n);
-
+double** creation_matrix( int n){
     double** matrix1 = (double**)malloc(n * sizeof(double*));
-    double** matrix2 = (double**)malloc(n * sizeof(double*));
-
-    printf("Vvedite elementi pervoi matrizi: ");
-    for (i = 0; i < n; i++) {
+    printf("Vvedite elementi matrizi: \n");
+    for (int i = 0; i < n; i++) {
         matrix1[i] = (double*)malloc(n * sizeof(double));
-        for (j = 0; j < n; j++) {
+        for (int j = 0; j < n; j++) {
             scanf("%lf", &matrix1[i][j]);
         }
     }
+    return matrix1;
+}
 
-    printf("Vvedite elementi vtoroi matrizi: ");
-    for (i = 0; i < n; i++) {
-        matrix2[i] = (double*)malloc(n * sizeof(double));
-        for (j = 0; j < n; j++) {
-            scanf("%lf", &matrix2[i][j]);
-        }
-    }
-
+void print_matrix(int n,double** matrix1, double** matrix2){
+    char operation;
     printf("Vvedite operaziu (+, -, *): ");
     scanf(" %c", &operation);
-
     double** result = matrix_calculator(matrix1, matrix2, n, operation);
-
     printf("rezultat operazii: \n");
-    for (i = 0; i < n; i++) {
-        for (j = 0; j < n; j++) {
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
             printf("%.0f ", result[i][j]);
         }
         printf("\n");
     }
+}
+
+int main() {
+    int n,i,j;
+    
+    printf("vvedite razmernost' matrizi n: ");
+    scanf("%d", &n);
+
+    double** matrix1 = creation_matrix( n);
+    double** matrix2 = creation_matrix( n);
+
+    print_matrix(n,matrix1,matrix2);
 
     for (i = 0; i < n; i++) {
         free(matrix1[i]);
         free(matrix2[i]);
-        free(result[i]);
     }
     free(matrix1);
     free(matrix2);
-    free(result);
 
     return 0;
 }
