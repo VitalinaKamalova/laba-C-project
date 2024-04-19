3ac879e108aa0130f4b34d9279cfbdacf85d5946
Vitalina <linakimarmy@mail.ru>
2024-04-19 20:12:54 +1000

    Update laba 7.c

--- laba 7.c
+++ laba 7.c
@@ -1,3 +1,14 @@
 #include <stdio.h>
 #include <string.h>
 
+struct humen{
+	char name[30],surname[30];
+	int year;
+};
+
+int main(){
+	struct humen f[n], s[n], y;
+	char name[30], surname[30];
+	int n, year;
+	
+}
