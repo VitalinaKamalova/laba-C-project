66811edcaa27c31eb487359553b0a6159558c5f1
Vitalina <linakimarmy@mail.ru>
2024-04-19 20:40:39 +1000

    Update laba 7.c

--- laba 7.c
+++ laba 7.c
@@ -7,8 +7,18 @@ struct humen{
 };
 
 int main(){
+	int n = 4;
 	struct humen f[n], s[n], y;
 	char name[30], surname[30];
-	int n, year;
+	int year;
 	
+	for(int i =0; i<n; i++){
+		print("Enter the %d person's name: ", i+1);
+		scanf("%s", name)
+		print("Enter the %d person's surname: ", i+1);
+		scanf("%s", surname)
+		print("Enter the %d person's year of birth: ", i+1);
+		scanf("%d", year)
+		
+	}
 }
