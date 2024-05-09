3cbc12e3e430385a38226154b78c8ca9f05906f9
Vitalina <linakimarmy@mail.ru>
2024-05-10 00:55:18 +1000

    Update laba 7.c

--- laba 7.c
+++ laba 7.c
@@ -2,8 +2,8 @@
 #include <string.h>
 
 struct humen{
-  char name[30],surname[30];
-  int year;
+  char name[30],surname[30], sex[30];
+  int year, hight;
 };
 
 int main(){
@@ -12,8 +12,8 @@ int main(){
   int i,j;
   
   for( i=0; i < n; i++){
-    printf("Enter the %d person's name, surname and date of birth: ", i+1);
-    scanf("%s %s %d", p[i].name, p[i].surname, &p[i].year);
+    printf("Enter the %d person's name, surname, date of birth, sex and hihght: ", i+1);
+    scanf("%s %s %d %s %d", p[i].name, p[i].surname, &p[i].year, p[i].sex, &p[i].hight);
   }
   /*
   FILE *file;
