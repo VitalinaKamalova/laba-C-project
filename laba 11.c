805192e20d65ec08d7e52ca6328f09e29008905d
Vitalina <linakimarmy@mail.ru>
2024-05-30 21:24:48 +1000

    Update laba 11.c

--- laba 11.c
+++ laba 11.c
@@ -1,3 +1,21 @@
 #include <time.h>
 #include <stdio.h>   
 #include <stdarg.h>
+
+int sum(int num_args, ...)  
+{
+    va_list args;
+    va_start(args, num_args);
+    
+    int total = 0;
+    for (int i = 0; i < num_args; i++)
+    {
+        total += va_arg(args, int);
+    }
+    
+    va_end(args);
+    
+    return total;
+}
+
+
