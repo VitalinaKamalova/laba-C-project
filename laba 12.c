b5e3ebc7826e0fdca818fd8397e429c7adb4d4de
Vitalina <linakimarmy@mail.ru>
2024-05-31 21:53:41 +1000

    Update laba 12.c

--- laba 12.c
+++ laba 12.c
@@ -1,3 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+
+// функция для определения дня недели
+int getDayOfWeek(int year, int month, int day) {
+    struct tm date = {0};
+    date.tm_year = year - 1900;
+    date.tm_mon = month - 1;
+    date.tm_mday = day;
+    mktime(&date);
+    return date.tm_wday;
+}
+
+
