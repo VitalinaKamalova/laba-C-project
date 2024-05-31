f250c7db9029a2691d3a61c1e59d83019140da2b
Vitalina <linakimarmy@mail.ru>
2024-05-31 22:51:44 +1000

    Create laba 13.c

---
+++ laba 13.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <signal.h>
+
+void new_sigabrt(int sig){
+    printf("Oh no! SIGABRT!");
+    exit(1);
+}
+
+void new_sigabrt(int sig){
+    printf("Oh no! SIGABRT!");
+    exit(1);
+}
+
+void new_sigabrt(int sig){
+    printf("Oh no! SIGABRT!");
+    exit(1);
+}
+
+
